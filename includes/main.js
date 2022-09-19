5c99a3e4bb0383230bb3d9a0e1bba31188391218
Prakash8782 <prakash8782@gmail.com>
2022-09-19 14:47:43 +0530

    includes

---
+++ includes/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : " ,colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.includes()");
+let a=colors.includes("Red");
+console.log("change in array : ",a);
+console.log("return : length of array after includes(); is "+colors.length);
